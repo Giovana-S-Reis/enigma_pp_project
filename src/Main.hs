521b0530d3834cc076a2d7b1e751388a11f44673
Giovana-S-Reis <84756749+Giovana-S-Reis@users.noreply.github.com>
2023-08-28 18:43:21 -0300

    Add files via upload

--- src/Main.hs
+++ src/Main.hs
@@ -6,26 +6,22 @@ import Control.Applicative
 import Path_paises
 import Cesar
 import System.Timeout (timeout)
+import Graphics.Gloss.Interface.Pure.Game
 
 --Declaração do número máximo de erros
 numeroMaxErros :: Int
 numeroMaxErros = 8
 
---Esccolhe o valor do shift aplicado
-{-randomNumber :: IO Int
-randomNumber = randomRIO (1, 26)
-
-num <- randomNumber
--}
-
-
 mostrarPalavra :: String -> String
 mostrarPalavra palavra = encode 3 palavra
 
-tentarLetra :: String -> Char -> Int -> IO ()
-tentarLetra palavra letra tentativas
-	| letra `elem` palavra 	= jogo [if letra == a then toUpper letra else a | a <- palavra] tentativas
-	| otherwise = jogo palavra (tentativas -1)
+tentarLetraIO :: String -> Char -> Int -> IO GameState
+tentarLetraIO palavra letra tentativas
+    | letra `elem` palavra = jogo (atualizaPalavra palavra letra) tentativas
+    | otherwise = jogo palavra (tentativas - 1)
+    where
+        atualizaPalavra :: String -> Char -> String
+        atualizaPalavra palavra char = [if a == char then toUpper a else a | a <- palavra]
 
 -- Esta funcao faz um sorteio de uma palvra dentro de um arquivo .txt de forma aleatoria
 sorteiaPalavra :: IO[Char]
@@ -52,14 +48,16 @@ adivinharPalavra palavra tentativa = return (map toUpper palavra == map toUpper 
 
 
 -- Funcao que realiza o jogo e o loop do jogo, verificando se ainda restam numero de tentativas
-jogo :: String -> Int -> IO ()
+jogo :: String -> Int -> IO GameState
 jogo palavra tentativas
     | palavra == map toUpper palavra = do
         putStrLn $ mostrarPalavra palavra
-        putStrLn ("Voce Ganhou! "++ map toUpper palavra ++ " está livre do bombardeio.") 
+        putStrLn ("Voce Ganhou! "++ map toUpper palavra ++ " está livre do bombardeio.")
+        return (initialState "" 0)
     | tentativas == 0 = do
         putStrLn $ mostrarPalavra palavra
         putStrLn ("Voce Perdeu..." ++ map toUpper palavra ++ " sofreu um bombardeio.")
+        return (initialState "" 0)
     | otherwise = do
         putStrLn $ "Voce tem " ++ show tentativas ++ " tentativas restantes."
         putStrLn $ mostrarPalavra palavra
@@ -70,18 +68,75 @@ jogo palavra tentativas
                 putStrLn "Nenhuma letra digitada. Por favor, tente novamente."
                 jogo palavra tentativas
             else if length tentativaDeLetra == 1
-                then do
-                    tentarLetra palavra (head tentativaDeLetra) tentativas
+                then tentarLetraIO palavra (head tentativaDeLetra) tentativas
                 else do
                     acertouPalavra <- adivinharPalavra palavra tentativaDeLetra
                     if acertouPalavra
-                        then putStrLn ("Voce Ganhou! "++ map toUpper palavra ++ " está livre do bombardeio.") 
+                        then do
+                            putStrLn ("Voce Ganhou! "++ map toUpper palavra ++ " está livre do bombardeio.")
+                            return (initialState "" 0)
                         else do
                             putStrLn ("Tentativa incorreta! " ++ map toUpper palavra ++ " sofreu um bombardeio.")
+                            return (initialState "" (tentativas - 1))
+
+
+data GameState = GameState
+    { palavraSecreta :: String
+    , tentativasRestantes :: Int
+    , inputAtual :: String
+    }
+
+initialState :: String -> Int -> GameState
+initialState palavra tentativas = GameState
+    { palavraSecreta = palavra
+    , tentativasRestantes = tentativas
+    , inputAtual = ""
+    }
+
+window :: Display
+window = InWindow "Enigma Game" (800, 600) (10, 10)
+
+background :: Color
+background = white
+
+drawGame :: GameState -> Picture
+drawGame gameState = pictures
+    [ translate (-500) 0 $ scale 0.5 0.5 $ text ("Palavra: " ++ mostrarPalavra (palavraSecreta gameState))
+    , translate (-200) (-50) $ scale 0.5 0.5 $ text ("Tentativas restantes: " ++ show (tentativasRestantes gameState))
+    , translate (-200) (-100) $ scale 0.5 0.5 $ text "Digite uma letra ou tente adivinhar o nome do país:"
+    , translate (-200) (-150) $ scale 0.5 0.5 $ text ("Input atual: " ++ inputAtual gameState)
+    ]
+
+processInput :: GameState -> String -> GameState
+processInput gameState input =
+    case input of
+        [c] -> gameState { inputAtual = "" }  -- Limpa o input após a tentativa de adivinhação
+        _   -> gameState { palavraSecreta = "", inputAtual = "" }
+
+
+handleInput :: Event -> GameState -> GameState
+handleInput (EventKey (Char c) Down _ _) gameState =
+    gameState { inputAtual = inputAtual gameState ++ [c] }
+handleInput (EventKey (SpecialKey KeyEnter) Down _ _) gameState =
+    if null (inputAtual gameState)
+        then gameState
+        else processInput gameState (inputAtual gameState)
+handleInput _ gameState = gameState
+
+
+updateGame :: Float -> GameState -> GameState
+updateGame _ gameState = gameState
+
+main :: IO ()
+main = do
+    putStrLn "Bem vindo ao Enigma!"
+    palavra <- sorteiaPalavra
+    let initialState' = initialState palavra numeroMaxErros
+    play window background 60 initialState' drawGame handleInput updateGame
 
 
 -- Inicia o jogo
-main :: IO ()
+{-main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering
     putStrLn "Bem vindo ao Enigma!\nO Exercito Nazista decidiu bombardear um país. Com sorte interceptamos a mensagem com o nome! Agora o seu dever é decodificar a mensagem antes que o bombardeio ocorra!\nMas cuidado, suspeitamos de um espião entre nós. É possível que sempre que você adivinhe uma letra a criptografia mude."
@@ -96,4 +151,5 @@ main = do
     -- Verifique o resultado do jogo
     case mResultado of
         Just () -> putStrLn "Obrigado por jogar! :)"
-        Nothing -> putStrLn ("Tempo esgotado! " ++ map toUpper palavra ++ " sofreu um bombardeio.")
+        Nothing -> putStrLn ("Tempo esgotado! " ++ palavra ++ " sofreu um bombardeio.")
+-}
